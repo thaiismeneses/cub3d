fea390cc1214f7d517178ffc6c1262872a1d1aaf
Luíza <luizafuruno@gmail.com>
2025-01-30 09:04:11 -0300

    Wall utils

--- src/val_wall.c
+++ src/val_wall.c
@@ -12,55 +12,6 @@
 
 #include "../includes/cub3d.h"
 
-static void full_matrix(char **matrix, t_token *tokens)
-{
-    int i;
-    t_token *current;
-
-    i = 0;
-    current = tokens;
-    while (current != NULL)
-    {
-        if (current->type == MAP)
-        {
-            matrix[i] = ft_strdup(current->data);
-            if (!matrix[i])
-                break ;
-            i++;
-        }
-        current = current->next;
-    }
-}
-
-char **map_to_matrix(t_token *tokens)
-{
-    int line_count;
-    int max_cols;
-    int len;
-    char **matrix;
-    t_token *current;
-
-    current = tokens;
-    line_count = 0;
-    max_cols = 0;
-    while (current != NULL)
-    {
-        if (current->type == MAP)
-        {
-            line_count++;
-            len = ft_strlen(current->data);
-            if (len > max_cols)
-                max_cols = len;
-        }
-        current = current->next;
-    }
-    matrix = malloc(line_count * sizeof(char *));
-    if (!matrix)
-        return (NULL);
-    full_matrix(matrix, tokens);
-    return (matrix);
-}
-
 static int verify_location(char **map, int i, int j)
 {
     if ((i > 0 && (map[i - 1][j] == '1' || map[i - 1][j] == ' ')) ||
